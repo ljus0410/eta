9e689d92cec2cb50c3c9d01e6c6eee6b3e051170
minsu kim <als9045@naver.com>
2023-12-07 16:12:52 +0900

    Fix : redis import 수정

--- src/main/java/kr/pe/eta/service/user/impl/UserServiceImpl.java
+++ src/main/java/kr/pe/eta/service/user/impl/UserServiceImpl.java
@@ -1,11 +1,12 @@
 package kr.pe.eta.service.user.impl;
 
-import java.awt.Point;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.geo.Point;
 import org.springframework.stereotype.Service;
 
 import kr.pe.eta.common.Search;
